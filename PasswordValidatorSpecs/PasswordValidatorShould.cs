8415211b76a1d991f887fd42ef9140f7041d42f9
Isa Pérez <isperez@aidacanarias.com>
2022-04-26 17:46:25 +0200

    🧪 New test for the case when password is shorter than 8 characters

--- PasswordValidatorSpecs/PasswordValidatorShould.cs
+++ PasswordValidatorSpecs/PasswordValidatorShould.cs
@@ -10,9 +10,29 @@ namespace PasswordValidatorSpecs
         }
 
         [Test]
-        public void Test1()
+        public void should_return_validation_error_when_password_is_shorter_than_8_characters()
         {
-            Assert.Pass();
+            var password = "a";
+            var passwordValidator = new PasswordValidator();
+
+            var validationResult = passwordValidator.Validate(password);
+
+            Assert.IsTrue(validationResult.IsValid);
+            Assert.IsTrue(validationResult.ErrorMessage.Contains("Password must be at least 8 characters"));
         }
+    }
+
+    public class PasswordValidator
+    {
+        public ValidationResult Validate(string password)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class ValidationResult
+    {
+        public bool IsValid { get; set; }
+        public string ErrorMessage { get; set; }
     }
 }
